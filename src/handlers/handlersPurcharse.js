4e371e7b065814a62cec9761489db1ca7a3bf66d
lucas1visa <visalucas557@gmail.com>
2023-08-15 16:26:25 -0300

    x

--- src/handlers/handlersPurcharse.js
+++ src/handlers/handlersPurcharse.js
@@ -2,11 +2,11 @@ const {postPay} = require("../controllers/controllerPurcharse")
 const postPurcharse = async (req, res) => {
     const {payment_id,payment_type,status} = req.query; 
     const {quantity,userId,idProduct}= req.body
+    console.log(userId,payment_id)
     try {
-      if (userId) {
         result = await postPay(payment_id,payment_type,status,userId,quantity,idProduct)
-       return res.status(200).send("Exito al cargar datos.");
-    }
+      res.status(200).send("Exito al cargar datos.");
+
     } catch (error) {
       res.status(500).send(error);
     }
