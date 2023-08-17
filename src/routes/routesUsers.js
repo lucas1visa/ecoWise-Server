3c288612e37493d744ce110c97f6dc0b91e58c67
lucas1visa <visalucas557@gmail.com>
2023-08-17 19:39:54 -0300

    s

--- src/routes/routesUsers.js
+++ src/routes/routesUsers.js
@@ -1,9 +1,8 @@
 const { Router } = require("express");
-const {getUsers,postUsers,putUsers, deleteUsers,postUserAddress} = require("../handlers/handlersUsers")
+const {getUsers,postUsers,putUsers, deleteUsers} = require("../handlers/handlersUsers")
 const  routesUsers = Router();
 routesUsers.get("/" , getUsers)
 routesUsers.post("/" , postUsers)
-routesUsers.post("/" , postUserAddress)
 routesUsers.put("/update", putUsers)
 routesUsers.delete("/delete/:id", deleteUsers)
 module.exports = routesUsers;
