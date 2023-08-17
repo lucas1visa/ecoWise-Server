f6c0b325202ee4a9d8f8271c8c68d8f4c86fbafd
lucas1visa <visalucas557@gmail.com>
2023-08-17 19:30:40 -0300

    d

--- src/controllers/controllersCart.js
+++ src/controllers/controllersCart.js
@@ -15,18 +15,35 @@ const getcarrito = async ()=>{
     }
 }
 
-const crearCart = async (id,UserId)=>{
-    try {
-    const nuevoProductoCarrito = await Cart.create({
+const crearCart = async (id, UserId, carrito) => {
+  try {
+    if (id) {
+      const nuevoProductoCarrito = await Cart.create({
         UserId
       });
-      await nuevoProductoCarrito.setProducts(id)
+
+      await nuevoProductoCarrito.setProducts(id);
       return nuevoProductoCarrito;
-    } catch (error) {
-      console.error("Error el producto al carrito de compras:", error);
-      return null;
+    } else {
+      const nuevosCarritos = await Cart.create({
+        UserId
+      });
+      // Crear un nuevo objeto Cart para manejar los productos del carrito
+      const nuevoCarrito = await Cart.create({
+        UserId
+      });
+      // Iterar sobre los productos en carrito y agregarlos al nuevo carrito
+      carrito.forEach(async (producto) => {
+        await nuevoCarrito.setProducts(producto.id);
+      });
+
+      return nuevoCarrito;
     }
-}
+  } catch (error) {
+    console.error("Error al agregar el producto al carrito de compras:", error);
+    return null;
+  }
+};
 
 // no incluyo el nombre porque siempre va a ser el mismo nombre
 const updateCarrito = async (  id, price, quantityAvailable, image)=>{
