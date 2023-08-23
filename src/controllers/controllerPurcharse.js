571dcc84165dac7b91a0c358ed3080e45c0b4262
lucas1visa <visalucas557@gmail.com>
2023-08-23 13:58:54 -0300

    .

--- src/controllers/controllerPurcharse.js
+++ src/controllers/controllerPurcharse.js
@@ -1,24 +1,28 @@
 const { Purchase, Product,Users } = require("../db");
-const postPay = async (payment_id, payment_type, status, userId, quantity, idProduct) => {
+const postPay = async (payment_id, payment_type, status, compras) => {
   try {
-    const product = await Product.findByPk(idProduct);
-
-    if (!product) {
+    compras.forEach(async e => {
+      
+      const product = await Product.findByPk(e.productId);
+      
+      if (!product) {
       throw new Error("Producto no encontrado");
     }
     const purchase = await Purchase.create({
         payment_id:payment_id,
-      payment_type: payment_type,
-      status:status,
-      UserId: userId,
-      quantity:quantity
+      payment_type:payment_type,
+      status: status,
+      UserId: e.userId,
+      quantity:e.cantidad,
+      total:e.precio
     });
     const currentQuantity =  await product.quantityAvailable;
-    const newQuantity = currentQuantity - quantity;
+    const newQuantity = currentQuantity - e.cantidad;
     // Actualizar la cantidad en la entrada existente
     await product.update({ quantityAvailable: newQuantity });
-    await purchase.setProducts(idProduct)
+    await purchase.setProducts(e.productId)
     return purchase;
+  });
   } catch (error) {
     throw new Error("Error al procesar la compra: " + error.message);
   }
--- src/handlers/handlersPurcharse.js
+++ src/handlers/handlersPurcharse.js
@@ -3,22 +3,10 @@ const transporter = require("../utils/mailer");
 const { User } = require("../db");
 
 const postPurcharse = async (req, res) => {
-  const { quantity, userId, idProduct, payment_id, payment_type, status } = req.body;
+  const { compras,payment_id, payment_type, status} = req.body;
   try {
-    const result = await postPay(payment_id, payment_type, status, userId, quantity, idProduct);
+    const result = await postPay(payment_id, payment_type, status, compras);
     
-    const user = await User.findByPk(userId);
-
-    if (!user) {
-      throw new Error("Usuario no encontrado");
-    }
-
-    const emailUser = user.email;
-
-    // Assuming result includes the associated product details
-    
-    
-    await transporter.sendPurchaseConfirmationEmail(emailUser); // Include purchaseDetails here
     
     res.status(200).send("Exito al cargar datos.");
   } catch (error) {
