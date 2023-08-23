8fe972b795fdc5e10de82e567ddf79a0b441b083
lucas1visa <visalucas557@gmail.com>
2023-08-23 18:05:15 -0300

    .

--- src/handlers/handlersCart.js
+++ src/handlers/handlersCart.js
@@ -1,6 +1,8 @@
 const {getcarrito,crearCart,updateCarrito, deletCart}=require("../controllers/controllersCart")
     const getCart = async (req, res) => {
-        const {UserId} = req.body
+
+        const {UserId} = req.query
+        console.log(UserId)
         try {
             const Carito = await getcarrito(UserId)
             res.status(200).send(Carito)
