ef494373f3eeba1a3f403e48687d928119f75228
DAC <d-ardila@outlook.com>
2023-08-23 20:53:16 -0500

    atras

--- src/controllers/controllerPurcharse.js
+++ src/controllers/controllerPurcharse.js
@@ -1,37 +1,28 @@
-const { Purchase, Product, Users } = require("../db");
-const transporter = require("../utils/mailer"); // Import the email sending utility
-
-const postPay = async (payment_id, payment_type, status, compras, emailUser) => {
+const { Purchase, Product,Users } = require("../db");
+const postPay = async (payment_id, payment_type, status, compras) => {
   try {
-    const purchases = await Promise.all(compras.map(async e => {
+    compras.forEach(async e => {
+      
       const product = await Product.findByPk(e.productId);
-
+      
       if (!product) {
-        throw new Error("Producto no encontrado");
-      }
-
-      const purchase = await Purchase.create({
-        payment_id: payment_id,
-        payment_type: payment_type,
-        status: status,
-        UserId: e.userId,
-        quantity: e.cantidad,
-        total: e.precio
-      });
-
-      const currentQuantity = await product.quantityAvailable;
-      const newQuantity = currentQuantity - e.cantidad;
-
-      await product.update({ quantityAvailable: newQuantity });
-      await purchase.setProducts(e.productId);
-
-      return purchase;
-    }));
-
-    // Send email here
-    await transporter.sendPurchaseConfirmationEmail(emailUser);
-
-    return purchases;
+      throw new Error("Producto no encontrado");
+    }
+    const purchase = await Purchase.create({
+        payment_id:payment_id,
+      payment_type:payment_type,
+      status: status,
+      UserId: e.userId,
+      quantity:e.cantidad,
+      total:e.precio
+    });
+    const currentQuantity =  await product.quantityAvailable;
+    const newQuantity = currentQuantity - e.cantidad;
+    // Actualizar la cantidad en la entrada existente
+    await product.update({ quantityAvailable: newQuantity });
+    await purchase.setProducts(e.productId)
+    return purchase;
+  });
   } catch (error) {
     throw new Error("Error al procesar la compra: " + error.message);
   }
--- src/handlers/handlersPurcharse.js
+++ src/handlers/handlersPurcharse.js
@@ -1,25 +1,17 @@
 const { postPay } = require("../controllers/controllerPurcharse");
+const transporter = require("../utils/mailer");
 const { User } = require("../db");
 
 const postPurcharse = async (req, res) => {
-  const { compras, payment_id, payment_type, status } = req.body;
+  const { compras,payment_id, payment_type, status} = req.body;
   try {
-    const user = await User.findByPk(compras);
-
-    if (!user) {
-      throw new Error("Usuario no encontrado");
-    }
-
-    const emailUser = user.email;
-
-    const result = await postPay(payment_id, payment_type, status, compras, emailUser);
-
+    const result = await postPay(payment_id, payment_type, status, compras);
+    
+    
     res.status(200).send("Exito al cargar datos.");
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
-module.exports = {
-  postPurcharse
-};
+module.exports = { postPurcharse };
